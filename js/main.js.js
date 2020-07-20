ee8b7631fd58ecc043e2f3c75a80738b0599b0a8
PavloMarchenko <dksab33@gmail.com>
2020-07-20 20:16:11 +0300

    20.07.20

--- js/main.js.js
+++ js/main.js.js
@@ -17,3 +17,28 @@ $(window).on("scroll" , function() {
         $('.navbar-light .navbar-toggler').removeClass('black');
     }
 })
+
+
+(function() {
+    const formEl = document.querySelector("#contactForm");
+    formEl.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const body = {
+            name: event.target.name.value,
+            email: event.target.email.value,
+            message: event.target.message.value
+        }
+
+        fetch("https://stoliari-node.herokuapp.com/api", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"  
+            },
+            body: JSON.stringify(body)
+        }).then(r => r.text()).then(response => {
+            // TODO add alert about successful submit 
+            console.log(response, "from server")
+        });
+    });
+})();
